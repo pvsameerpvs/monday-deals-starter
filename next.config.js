174327c0878166abac5c4e74d34e22d2a4678b4b
pvsameerpvs <pvsameerpvs@gmail.com>
2025-08-17 17:01:34 +0400

    fix: gh-pages basePath

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
+  basePath: "/monday-deals-starter",
+  assetPrefix: "/monday-deals-starter/",
   experimental: {
-    typedRoutes: true
-  }
+    typedRoutes: true,
+  },
 };
 
 module.exports = nextConfig;
